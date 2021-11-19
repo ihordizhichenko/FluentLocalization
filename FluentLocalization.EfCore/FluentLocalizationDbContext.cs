c5cc1bd69f97aeaf5ddfd6d95452bf25057408df
Ihor Dizhichenko <ihor.dizhichenko@agiliway.com>
2021-11-19 16:51:45 +0200

    Clean up entites

--- FluentLocalization.EfCore/FluentLocalizationDbContext.cs
+++ FluentLocalization.EfCore/FluentLocalizationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using FluentLocalization.EfCore.Entites;
+﻿using FluentLocalization.EfCore.Configurations;
+using FluentLocalization.EfCore.Entites;
 using Microsoft.EntityFrameworkCore;
 
 namespace FluentLocalization.EfCore
@@ -6,5 +7,11 @@ namespace FluentLocalization.EfCore
     internal class FluentLocalizationDbContext : DbContext
     {
         public DbSet<PropertyLocalization> PropertyLocalizations { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new PropertyLocalizationTypeConfiguration());
+
+            base.OnModelCreating(builder);
+        }
     }
 }
